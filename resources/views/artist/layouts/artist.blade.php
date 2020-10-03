54c887db9d88175b733c24f81f43de377b34b301
lootoos <lootoos97@yahoo.com>
2020-10-03 12:25:55 +0330

    add errors to layout  artist.blade.php

--- resources/views/artist/layouts/artist.blade.php
+++ resources/views/artist/layouts/artist.blade.php
@@ -22,6 +22,28 @@
 <body class="tm-bg-dark">
 <main class="tm-container masonry">
 
+{{--    TODO: add errors to messages layout--}}
+
+{{--    @if(session('success'))--}}
+{{--        <div class="row mb-2">--}}
+{{--            <div class="col-md-12">--}}
+{{--                <div class="alert alert-success" role="alert">--}}
+{{--                    {{session('success')}}--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    @endif--}}
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @yield('content')
+
     @yield('content')
 
 
