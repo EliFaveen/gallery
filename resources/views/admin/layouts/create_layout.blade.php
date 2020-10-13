c7013d3e0c124998c324fb12564be1dec2fde9ec
lootoos <lootoos97@yahoo.com>
2020-10-13 10:55:40 +0330

    add nav admin to /inc/admin_nav.blade.php for all admin pages

--- resources/views/admin/layouts/create_layout.blade.php
+++ resources/views/admin/layouts/create_layout.blade.php
@@ -27,67 +27,7 @@
 <body class="bg02">
 <div class="container">
     <!-- nav bar -->
-    <div class="row">
-        <div class="col-12">
-            <nav class="navbar navbar-expand-xl navbar-light bg-light">
-                <a class="navbar-brand" href="index.html">
-                    <i class="fas fa-3x fa-tachometer-alt tm-site-icon"></i>
-                    <h1 class="tm-site-title mb-0">Dashboard</h1>
-                </a>
-                <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.html">Dashboard
-                                <span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="index.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
-                               aria-expanded="false">
-                                Reports
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Daily Report</a>
-                                <a class="dropdown-item" href="#">Weekly Report</a>
-                                <a class="dropdown-item" href="index.html">Yearly Report</a>
-                            </div>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="products.html">Products</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="accounts.html">Accounts</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
-                               aria-expanded="false">
-                                Settings
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Profile</a>
-                                <a class="dropdown-item" href="#">Billing</a>
-                                <a class="dropdown-item" href="#">Customize</a>
-                            </div>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link d-flex" href="login.html">
-                                <i class="far fa-user mr-2 tm-logout-icon"></i>
-                                <span>Logout</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </div>
+    @include('inc.admin_nav')
 
     <!-- row -->
     {{--    @if(session('success'))--}}
