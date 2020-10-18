b7bf4f88c665ef30e36a44b8b701c15ec2c166f1
lootoos <lootoos97@yahoo.com>
2020-10-18 18:41:17 +0330

    index.blade.php show posts in cards

--- resources/views/artist/pages/posts/index.blade.php
+++ resources/views/artist/pages/posts/index.blade.php
@@ -4,29 +4,52 @@
 @extends('artist.layouts.artist')
 
 @section('content')
+    @foreach($posts as $post)
+    <div class="card" style="width: 18rem;">
+        {{--        image         --}}
+{{--        @foreach($post->photos as $pics)--}}
+{{--            @if($pics->img_url)--}}
+{{--                <img style="width: 100px; height: 100px;" class="card-img-top img-thumbnail img-fluid" src="{{url($pics->img_url)}}" alt="{{$post->title}}">--}}
 
-    <div class="card-body"><h5 class="card-title">پست های من</h5>
-        <table class="mb-0 table">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>عنوان پست</th>
-                <th>توضیحات</th>
-                <th>تصاویر</th>
-                <th>ویرایش</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($posts as $post)
-                <tr>
-                    <th scope="row">{{$post->id}}</th>
-                    <td>{{$post->title}}</td>
-                    <td>{{$post->description}}</td>
+{{--            @else--}}
+{{--                --}}{{--Todo : else doesnt work--}}
+{{--                <img class="img-fluid img-thumbnail"  src="{{url('assets/artist/img/default_for_posts/image-01.jpg')}}" alt="Image" style="width: 100px; height: 100px;">--}}
+
+{{--            @endif--}}
+{{--        @endforeach--}}
+        @if($post->photos->first())
+            <img style="" class="card-img-top img-thumbnail img-fluid" src="{{url($post->photos->first()->img_url)}}" alt="{{$post->title}}">
+        @endif
+        <div class="card-body">
+            <h5 class="card-title">{{$post->title}}</h5>
+            <p class="card-text">{{$post->description}}</p>
+            <a href="#" class="btn btn-primary">Show Post</a>
+        </div>
+    </div>
+    @endforeach
+
+{{--    <div class="card-body"><h5 class="card-title">پست های من</h5>--}}
+{{--        <table class="mb-0 table">--}}
+{{--            <thead>--}}
+{{--            <tr>--}}
+{{--                <th>#</th>--}}
+{{--                <th>عنوان پست</th>--}}
+{{--                <th>توضیحات</th>--}}
+{{--                <th>تصاویر</th>--}}
+{{--                <th>ویرایش</th>--}}
+{{--            </tr>--}}
+{{--            </thead>--}}
+{{--            <tbody>--}}
+{{--            @foreach($posts as $post)--}}
+{{--                <tr>--}}
+{{--                    <th scope="row">{{$post->id}}</th>--}}
+{{--                    <td>{{$post->title}}</td>--}}
+{{--                    <td>{{$post->description}}</td>--}}
 {{--                    <td>--}}
-{{--                        --}}{{--                        {{\App\Category::find($product->category_id)->title}}--}}
+{{--                        --}}{{----}}{{--                        {{\App\Category::find($product->category_id)->title}}--}}
 {{--                        {{$product->category->title }}--}}
 {{--                    </td>--}}
-                    <td>
+{{--                    <td>--}}
 {{--                        @if(\App\Photo::where('post_id',$post->id) != null)--}}
 {{--                            @foreach(\App\Photo::where('post_id',$post->id)->get() as $pics)--}}
 
@@ -39,32 +62,32 @@
 
 {{--                         @endif--}}
 
-                            @foreach($post->photos as $pics)
-                                @if($pics->img_url)
-                                    <img style="width: 100px; height: 100px;" class="img-thumbnail img-fluid" src="{{url($pics->img_url)}}">
+{{--                            @foreach($post->photos as $pics)--}}
+{{--                                @if($pics->img_url)--}}
+{{--                                    <img style="width: 100px; height: 100px;" class="img-thumbnail img-fluid" src="{{url($pics->img_url)}}">--}}
 
-                                @else
-                                {{--Todo : else doesnt work--}}
-                                    <img class="img-fluid img-thumbnail"  src="{{url('assets/artist/img/default_for_posts/image-01.jpg')}}" alt="Image" style="width: 100px; height: 100px;">
+{{--                                @else--}}
+{{--                                --}}{{--Todo : else doesnt work--}}
+{{--                                    <img class="img-fluid img-thumbnail"  src="{{url('assets/artist/img/default_for_posts/image-01.jpg')}}" alt="Image" style="width: 100px; height: 100px;">--}}
 
-                                @endif
-                            @endforeach
+{{--                                @endif--}}
+{{--                            @endforeach--}}
 
-                    </td>
-                    <td>
-                        <a href="#"><i class="fa fa-pencil-alt"></i></a>
+{{--                    </td>--}}
+{{--                    <td>--}}
+{{--                        <a href="#"><i class="fa fa-pencil-alt"></i></a>--}}
 
-                        {{--                        <form method="post" action="/delete-type/{{$type->id}}">--}}
-                        {{--                            @csrf--}}
-                        {{--                            <button onclick="return confirm('sure?')" type="submit" class="btn btn-danger">delete</button>--}}
-                        {{--                        </form>--}}
-                    </td>
-                </tr>
-            @endforeach
+{{--                        --}}{{--                        <form method="post" action="/delete-type/{{$type->id}}">--}}
+{{--                        --}}{{--                            @csrf--}}
+{{--                        --}}{{--                            <button onclick="return confirm('sure?')" type="submit" class="btn btn-danger">delete</button>--}}
+{{--                        --}}{{--                        </form>--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--            @endforeach--}}
 
-            </tbody>
-        </table>
-    </div>
+{{--            </tbody>--}}
+{{--        </table>--}}
+{{--    </div>--}}
 
     <div class="row">
         <div class="col-md-12">
