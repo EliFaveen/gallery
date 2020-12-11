f79105cb3df3e56fc058b8b70e80306fda22fd9a
lootoos <lootoos97@yahoo.com>
2020-12-11 11:47:17 +0330

    add profile like to sidenav

--- resources/views/inc/sidenav.blade.php
+++ resources/views/inc/sidenav.blade.php
@@ -1,7 +1,19 @@
 {{--    sidenav col nadarad azad hast   --}}
 {{--    TODO: add to layout with if this is artist who looking at this page show sidenav--}}
 <div class="sidenav">
-{{--    <a href="{{route('artist.image-croppie')}}" class="all-sidenav plus"><i class="fas fa-plus"></i></a>--}}
+    @auth
+        <div class="small-profile-img-parent">
+            @if(auth()->user()->profile_pic)
+                <a href="{{route('artist.post.index')}}" class="all-sidenav user-img">
+                    <img src="{{url(auth()->user()->profile_pic)}}" alt="Avatar" class=" small-profile-img">
+                </a>
+            @else
+                <a href="{{route('artist.post.index')}}" class="all-sidenav user">
+                    <i class="fas fa-user-circle"></i>
+                </a>
+            @endif
+        </div>
+    @endauth
     <a href="{{route('artist.post.create')}}" class="all-sidenav plus"><i class="fas fa-plus"></i></a>
     <a href="#" class="all-sidenav heart"><i class="fas fa-heart"></i></a>
     <a href="{{route('artist.home.index')}}" class="all-sidenav home"><i class="fas fa-home"></i></a>
